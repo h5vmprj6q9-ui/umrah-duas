bcb8db4aedc044eadededda0727d3326cc7fcb45
h5vmprj6q9-ui <h5vmprj6q9@privaterelay.appleid.com>
2026-02-11 01:43:46 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE = "umrah-duas-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./sw.js"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  event.respondWith(
+    caches.match(req).then((cached) => cached || fetch(req).then((res) => {
+      // Optional: cache new GET requests as you browse
+      const copy = res.clone();
+      if (req.method === "GET" && req.url.startsWith(self.location.origin)) {
+        caches.open(CACHE).then(cache => cache.put(req, copy)).catch(()=>{});
+      }
+      return res;
+    }).catch(() => caches.match("./index.html")))
+  );
+});
